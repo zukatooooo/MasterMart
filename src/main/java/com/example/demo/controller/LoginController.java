7f9ac99486133d7e3b955160f16ed44f880f8e0f
zukatooooo <absandzezuka406@gmail.com>
2024-06-06 18:54:35 +0400

    first commit

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -4,6 +4,8 @@ import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,9 +30,9 @@ public class LoginController {
     public String processLogin(@RequestParam String username, @RequestParam String password, Model model) {
         User user = userRepository.findByName(username);
         if (user != null && Objects.equals(user.getPassword(), password)) {
-            return "redirect:/?username=" + username;
+            return "redirect:/?user=" + username;
         } else {
-            model.addAttribute("error", "Invalid username or password");
+            model.addAttribute("loginStatus", "Incorrect username or password");
             return "login";
         }
     }
